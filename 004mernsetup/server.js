bd9931691fdb7b546c699bffce92bb1793d53cab
Aidanwang0309 <aidanwang0309@gmail.com>
2019-01-25 02:28:35 -0500

    get rid of BodyParser

--- 004mernsetup/server.js
+++ 004mernsetup/server.js
@@ -1,13 +1,12 @@
 const express = require("express");
 const mongoose = require("mongoose");
-const bodyParser = require("body-parser");
 
 const items = require("./routes/api/items");
 
 const app = express();
 
 // bodyParser Middleware
-app.use(bodyParser.json());
+app.use(express.json());
 
 // DB Config
 const db = require("./config/keys").mongoURI;
